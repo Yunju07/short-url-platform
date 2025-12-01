dbf35daf389e08e4629e297fb808fdcb111e8b43
Yunju07 <juile0827@naver.com>
2025-12-02 00:20:16 +0900

    chore: 스프링 부트 기본 프로젝트 초기 구성 추가

---
+++ shorturl-app/src/main/java/com/yunju/shorturl_app/ShorturlAppApplication.java
@@ -0,0 +1,13 @@
+package com.yunju.shorturl_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShorturlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShorturlAppApplication.class, args);
+	}
+
+}
