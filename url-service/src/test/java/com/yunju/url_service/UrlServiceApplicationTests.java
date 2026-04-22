e1b3fce3e929f5531bfcaa273420a72228b0b651
Yunju07 <juile0827@naver.com>
2026-04-22 14:04:27 +0900

    fix: make redirect-service Kafka config test-friendly for CI

--- url-service/src/test/java/com/yunju/url_service/UrlServiceApplicationTests.java
+++ url-service/src/test/java/com/yunju/url_service/UrlServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yunju.redirect_service;
+package com.yunju.url_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,11 +7,10 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@EmbeddedKafka(partitions = 1, bootstrapServersProperty = "spring.kafka.bootstrap-servers")
-class RedirectServiceApplicationTests {
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
+class UrlServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
