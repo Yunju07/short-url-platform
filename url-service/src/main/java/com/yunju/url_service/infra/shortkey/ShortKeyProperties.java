99bf4ca0d727ee9e081f3978cae996262a85e32e
Yunju07 <juile0827@naver.com>
2026-04-23 17:33:34 +0900

    refactor: remove direct LLM API calls in preparation for FastAPI-based inference server

--- url-service/src/main/java/com/yunju/url_service/infra/shortkey/ShortKeyProperties.java
+++ url-service/src/main/java/com/yunju/url_service/infra/shortkey/ShortKeyProperties.java
@@ -11,15 +11,4 @@ public class ShortKeyProperties {
     private int length = 6;
     private String alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private int maxUniqueAttempts = 5;
-    private Llm llm = new Llm();
-
-    @Getter
-    @Setter
-    public static class Llm {
-        private boolean enabled = true;
-        private String baseUrl = "http://localhost:11434";
-        private String model = "llama3.1";
-        private int timeoutSeconds = 2;
-        private int maxAttempts = 3;
-    }
 }
